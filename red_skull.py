532c08435b86296a1dca2530e7803596a2d40944
Luke Nigro <lukenigro@Lukes-MBP.attlocal.net>
2023-02-06 18:19:34 -0500

    Added ability to call for locations to the bot

--- red_skull.py
+++ red_skull.py
@@ -3,7 +3,7 @@ import discord
 from discord import Embed
 from discord.ext import commands
 from dotenv import load_dotenv
-from get_cards import Card
+from snap_data import Card, Location
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -41,5 +41,29 @@ async def get_card(message, *, arg):
         await message.send(embed = embed)
     except:
         await message.send('That card doesn\'t exist you nimrod.')
+
+@bot.command(name='location')
+async def get_card(message, *, arg):
+    
+    correct_case = " ".join([word.capitalize() for word in arg.split()])
+
+    try:
+        loc = Location(correct_case)
+        embed.clear_fields()
+        embed.title = loc.loc_name
+        embed.color = discord.Color.blue()
+
+        ### TODO ADD MarvelSnapZone page links
+        # embed.url = loc.webpage
+        embed.add_field(name='Location', value=loc.loc_name, inline=False)
+        embed.add_field(name='Effect', value=loc.effect, inline=False)
+
+        ### TODO: ADD LOCATION IMAGE URLS TO JSON FILE
+        # embed.set_image(url=loc.image)
+        embed.set_footer(text='Created by unlimited_powah')
+
+        await message.send(embed = embed)
+    except:
+        await message.send('That location doesn\'t exist you jabroni.')
 
 bot.run(TOKEN)
--- snap_data.py
+++ snap_data.py
@@ -23,3 +23,23 @@ class Card():
             if " ".join([word.capitalize() for word in record['cname'].replace("-", " ").split()]) == card_name:
                 # found the record, print it
                 return record
+
+class Location():
+
+    def __init__(self, loc_name):
+        record = self.get_record(loc_name = loc_name)
+
+        self.loc_name = record['Location']
+        self.effect = record['Effect']
+
+    def get_record(self, loc_name):
+    # load the json file into memory
+        with open("data/locations.json", "r") as json_file:
+            data = json.load(json_file)
+
+        # loop through each record in the data
+        for record in data:
+            # check if the record has the given cname
+            if " ".join([word.capitalize() for word in record['Location'].replace("-", " ").split()]) == loc_name:
+                # found the record, print it
+                return record
