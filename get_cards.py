6749e91be6568065a77b3e267ce1c4aa4559fa55
Luke Nigro <lukenigro@Lukes-MBP.attlocal.net>
2023-02-06 14:05:19 -0500

    Initial Commit

---
+++ get_cards.py
@@ -0,0 +1,25 @@
+import json
+
+class Card():
+
+    def __init__(self, card_name):
+        record = self.get_record(card_name=card_name)
+
+        self.card_name = record['cname']
+        self.ability = record['ability']
+        self.cost = record['cost']
+        self.power = record['power']
+        self.image = record['art']
+        self.webpage = record['url']
+
+    def get_record(self, card_name):
+        # load the json file into memory
+        with open("data/cards.json", "r") as json_file:
+            data = json.load(json_file)
+
+        # loop through each record in the data
+        for record in data:
+            # check if the record has the given cname
+            if record["cname"] == card_name:
+                # found the record, print it
+                return record
---
+++ red_skull.py
@@ -0,0 +1,37 @@
+import os
+import discord
+from discord import Embed
+from discord.ext import commands
+from dotenv import load_dotenv
+from get_cards import Card
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+embed = Embed(title='Card Viewer')
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+@bot.command(name='skull')
+async def on_message(message):
+    quote = 'Red Skull will fuck your day up.'
+    await message.send(quote)
+
+@bot.command(name='card')
+async def get_card(message, *, arg):
+    
+    correct_case = " ".join([word.capitalize() for word in arg.split()])
+
+    try:
+        card = Card(correct_case)
+        embed.clear_fields()
+        embed.add_field(name='Ability', value=card.ability, inline=False)
+        embed.add_field(name='Cost', value=card.cost, inline=False)
+        embed.add_field(name='Power', value=card.power, inline=False)
+        embed.set_image(url=card.image)
+        embed.set_footer(text='Created by unlimited_powah')
+
+        await message.send(embed = embed)
+    except:
+        await message.send('That card doesn\'t exist you nimrod.')
+
+bot.run(TOKEN)
