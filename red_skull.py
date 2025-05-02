baa63a19a465a30d4a47a091c367814e27fab3c5
Luke Nigro <luke.nigro@outlook.com>
2025-05-02 19:27:08 -0400

    renamed functions so they didn't overlap because duh

--- red_skull.py
+++ red_skull.py
@@ -1,7 +1,6 @@
 import os
 import random
 import discord
-import asyncio
 from discord import Embed
 from discord.ext import commands
 
@@ -11,39 +10,39 @@ embed = Embed()
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.command(name='skull')
-async def on_message(message):
+async def on_skull_command(message):
     await message.send('Leave Red Skull alone, please. I beg of you.')
 
 @bot.command(name='cringe')
-async def on_message(message):  # noqa: F811
+async def on_cringe_command(message):
     await message.send('day')
 
 @bot.command(name='bonk')
-async def on_message(message):
+async def on_bonk_command(message):
     await message.send('https://media2.giphy.com/avatars/DogeBONK/coZ41g2NwRFS.gif')
 
 @bot.command(name='ban')
-async def on_message(message, *, arg):
+async def on_ban_command(message, *, arg):
     await message.send(f'User {arg} has been permanently banned from the server, what a loser! <:day:1072575755256598559>')
 
 @bot.command(name='boc')
-async def on_message(message):
+async def on_boc_command(message):
     await message.send("Claude told me you are wrong and that I am very smart. Trump is right about everything. My homeschooled kids will be totally normal.")
 
 @bot.command(name='squash')
-async def on_message(message):
+async def on_squash_command(message):
     await message.send("Yeah man I'm going to need you to go ahead and calm down for me. Can you do that?")
 
 @bot.command(name='hastings')
-async def on_message(message):
+async def on_hastings_command(message):
     await message.send("https://cdn.discordapp.com/attachments/1072494728492359732/1367960843660165200/8kxnz2icgq131.png?ex=68167c50&is=68152ad0&hm=2919a9cba02febadf631d53dd4ab50e1eb5a01f381c12a1cd8f67c13d42ec5c2&")
 
 @bot.command(name='kwiji')
-async def on_message(message):
+async def on_kwiji_command(message):
     await message.send("Your favorite ~~closeted Catholic boy~~ priest main!")
 
 @bot.command(name='roll')
-async def on_message(message, *, arg):
+async def on_roll_command(message, *, arg):
     if not arg.isdigit():
         await message.send("That's not a number you moron, pick a different number")
     elif int(arg) < 1:
@@ -53,7 +52,7 @@ async def on_message(message, *, arg):
         await message.send(f"You rolled a {roll}.")
 
 @bot.event
-async def on_message(message):
+async def on_cringe_message(message):
     await bot.process_commands(message)
     if 'cringe' in message.content.lower():
         roll = random.randint(1, 100)
@@ -61,7 +60,7 @@ async def on_message(message):
             await message.reply("you're cringe bro")
 
 @bot.event
-async def on_message(message):
+async def on_cheney_message(message):
     await bot.process_commands(message)
     if 'cheney' in message.content.lower():
         await message.reply("Shut up you idiot that’s not why the Dems lost the election.")
